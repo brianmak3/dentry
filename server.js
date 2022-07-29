c59f00c053bf2aa91c4e902b1fe8534118a56089
brianmak3 <brianworkss@gmail.com>
2022-07-29 19:13:33 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const { default: axios } = require('axios');
+const { manipulateSheet } = require('./src/utils');
+
+const express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    port = 4000,
+    bodyParser = require('body-parser'),
+    cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+app.post('/', (req, res) => {
+    manipulateSheet('create', req.body).then(data => {
+        manipulateSheet('get').then(data => {
+            res.status(201).json(data)
+        })
+    })
+})
+app.get('/', (req, res) => {
+    manipulateSheet('get').then(data => {
+        res.status(201).json(data.slice(1))
+    })
+})
+
+http.listen(port, () => {
+    console.log('App running  on port ' + port)
+})
+
+
+
+
+
