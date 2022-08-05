c27baf4dca0a1f19d1ed3c7acaa327cda297864b
brianmak3 <brianworkss@gmail.com>
2022-08-05 21:37:10 +0300

    files

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis'),
         keyFile: TOKEN_PATH,
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     }),
-    spreadsheetId = "1SMrpeJC2isCTJotRYXBDNENNbDVzCcazonOOwUQ-Vf0",
+    spreadsheetId = "1BxrqymyPqfF9DDorK8znrvzLZqd9jyRCgrGF9l9poqQ",
     manipulateSheet = (action, data) => new Promise(async (resolve, reject) => {
         let authClientObject = await auth.getClient(),
             googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject })
@@ -12,7 +12,7 @@ const { google } = require('googleapis'),
             let readData = await googleSheetsInstance.spreadsheets.values.get({
                 auth,
                 spreadsheetId,
-                range: "Sheet1!A:C",
+                range: "Sheet1!A:D",
             })
             resolve(readData.data.values)
         }
@@ -20,7 +20,7 @@ const { google } = require('googleapis'),
             let updateData = await googleSheetsInstance.spreadsheets.values.append({
                 auth,
                 spreadsheetId,
-                range: "Sheet1!A:C",
+                range: "Sheet1!A:D",
                 valueInputOption: 'RAW',
                 insertDataOption: 'INSERT_ROWS',
                 resource: {
