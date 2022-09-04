a2a6eb1d796b0913f3ea411681de166170e57d90
brianmak3 <brianworkss@gmail.com>
2022-09-04 20:48:58 +0300

    Addition of tabs

--- server.js
+++ server.js
@@ -12,20 +12,28 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 app.post('/', (req, res) => {
-    manipulateSheet('create', req.body).then(data => {
-        manipulateSheet('get').then(data => {
+    const { values, selectedTeam } = req.body
+    manipulateSheet('create', values, selectedTeam).then(data => {
+        manipulateSheet('get', null, selectedTeam).then(data => {
             res.status(201).json(data)
         })
     })
 })
 app.get('/', (req, res) => {
+    console.log(req.params)
     manipulateSheet('get').then(data => {
         res.status(201).json(data.slice(1))
+    })
+})
+app.get('/sheets', (req, res) => {
+    manipulateSheet('getSheets').then(data => {
+        res.status(201).json(data.map(a => a.properties.title))
     })
 })
 
 http.listen(port, () => {
     console.log('App running  on port ' + port)
+    manipulateSheet('getSheets')
 })
 
 
--- src/utils.js
+++ src/utils.js
@@ -5,14 +5,14 @@ const { google } = require('googleapis'),
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     }),
     spreadsheetId = "1SMrpeJC2isCTJotRYXBDNENNbDVzCcazonOOwUQ-Vf0",
-    manipulateSheet = (action, data) => new Promise(async (resolve, reject) => {
+    manipulateSheet = (action, data, sheet) => new Promise(async (resolve, reject) => {
         let authClientObject = await auth.getClient(),
             googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject })
         if (action === 'get') {
             let readData = await googleSheetsInstance.spreadsheets.values.get({
                 auth,
                 spreadsheetId,
-                range: "Sheet1!A:E",
+                range: `${sheet}!A:E`,
             })
             resolve(readData.data.values)
         }
@@ -20,15 +20,22 @@ const { google } = require('googleapis'),
             let updateData = await googleSheetsInstance.spreadsheets.values.append({
                 auth,
                 spreadsheetId,
-                range: "Sheet1!A:E",
+                range: `${sheet}!A:E`,
                 valueInputOption: 'RAW',
                 insertDataOption: 'INSERT_ROWS',
                 resource: {
                     values: [data],
                 },
-                includeValuesInResponse:true
+                includeValuesInResponse: true
             })
             resolve(updateData)
+        }
+        else if (action === 'getSheets') {
+            let spreadSheet = await googleSheetsInstance.spreadsheets.get({
+                auth,
+                spreadsheetId
+            })
+            resolve(spreadSheet.data.sheets)
         }
     })
 
